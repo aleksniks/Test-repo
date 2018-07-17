eaa44ee05b5b56c7319e414de36e932e9f537e95
Aleksandr Niks <aleksnik.sn@gmail.com>
2018-07-17 15:21:04 +0300

    added index.html and added h1 and p

---
+++ script.js
@@ -0,0 +1,6 @@
+<form>
+<SELECT NAME="sel" onchange="top.location.href = this.options[this.selectedIndex].value;">
+<option selected value="#">Ваш выбор</option>
+<option VALUE="http://kdg.HtmlWeb.ru">Home Page Author</option>
+<option VALUE="http://www.webclub.ru/">Клуб WEBмастеров</option>
+<option VALUE="http://www.citforum.ru/">Море аналитической информации</option></select></form>
